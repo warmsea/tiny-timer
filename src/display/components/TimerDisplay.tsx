bf7b07c238332d63a555e2d0c052b4f267752ab6
Su Su <s@warmsea.net>
2021-02-23 23:23:37 +0800

    Add unit tests for display logic (#4)
    * Setup Jest
    
    * Add unit tests for TimerDisplay

---
+++ src/display/components/TimerDisplay.tsx
@@ -0,0 +1,55 @@
+import { Box } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+
+import { IBridge } from '../../common/Bridge';
+import { Channel } from '../../common/Channel';
+
+export function getDisplayNumber(seconds: number): number {
+  if (Math.abs(seconds) >= 3600) {
+    return Math.ceil(seconds / 60);
+  } else {
+    return seconds;
+  }
+}
+
+export const ColorStyle = {
+  Normal: { bgcolor: "white", color: 'green' },
+  Warn: { bgcolor: 'yellow', color: 'black' },
+  TimeOut: { bgcolor: 'red', color: 'white' },
+};
+
+export function getColorStyle(remainingSeconds: number): { bgcolor: string, color: string } {
+  if (remainingSeconds > 30) {
+    return ColorStyle.Normal;
+  } else if (remainingSeconds > 0) {
+    return ColorStyle.Warn;
+  } else {
+    return ColorStyle.TimeOut;
+  }
+}
+
+export interface ITimerDisplayProps {
+  bridge: IBridge
+}
+
+export const TimerDisplay = (props: ITimerDisplayProps) => {
+  const [remainingSeconds, setRemainingSeconds] = useState(0);
+
+  useEffect(() => {
+    props.bridge.on(Channel.Tick, (_, seconds) => {
+      setRemainingSeconds(seconds);
+    });
+  }, []);
+
+  let style = getColorStyle(remainingSeconds);
+  let displayNumber = getDisplayNumber(remainingSeconds);
+  const sign = displayNumber < 0 ? <span className='sign'>-</span> : <></>;
+  displayNumber = Math.abs(displayNumber);
+  const firstPart = <span>{Math.floor(displayNumber / 60).toString().padStart(2, '0')}</span>;
+  const secondPart = <span>{Math.ceil(displayNumber % 60).toString().padStart(2, '0')}</span>;
+  return (
+    <Box bgcolor={style.bgcolor} color={style.color} >
+      {sign}{firstPart}<span className="separator">:</span>{secondPart}
+    </Box>
+  );
+}
--- src/display/display.tsx
+++ src/display/display.tsx
@@ -1,57 +1,17 @@
-import { Box, CssBaseline } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+import { CssBaseline } from '@material-ui/core';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Bridge } from '../common/Bridge';
-import { Channel } from '../common/Channel';
+import { TimerDisplay } from './components/TimerDisplay';
 
 import './display.scss';
-
-function getDisplayNumber(seconds: number): number {
-  if (Math.abs(seconds) >= 3600) {
-    return Math.ceil(seconds / 60);
-  } else {
-    return seconds;
-  }
-}
-
-function getStyle(remainingSeconds: number): { bgcolor: string, color: string } {
-  if (remainingSeconds > 30) {
-    return { bgcolor: "white", color: 'green' };
-  } else if (remainingSeconds > 0) {
-    return { bgcolor: 'yellow', color: 'black' };
-  } else {
-    return { bgcolor: 'red', color: 'white' };
-  }
-}
 
 window.onload = () => {
   ReactDOM.render((
     <>
       <CssBaseline />
-      <TimerDisplay />
+      <TimerDisplay bridge={Bridge} />
     </>
   ), document.getElementById('display'));
 };
-
-const TimerDisplay = () => {
-  const [remainingSeconds, setRemainingSeconds] = useState(0);
-
-  useEffect(() => {
-    Bridge.on(Channel.Tick, (_, seconds) => {
-      setRemainingSeconds(seconds);
-    });
-  }, []);
-
-  let style = getStyle(remainingSeconds);
-  let displayNumber = getDisplayNumber(remainingSeconds);
-  const sign = displayNumber < 0 ? <span className='sign'>-</span> : <></>;
-  displayNumber = Math.abs(displayNumber);
-  const firstPart = <span>{Math.floor(displayNumber / 60).toString().padStart(2, '0')}</span>;
-  const secondPart = <span>{Math.ceil(displayNumber % 60).toString().padStart(2, '0')}</span>;
-  return (
-    <Box bgcolor={style.bgcolor} color={style.color} >
-      {sign}{firstPart}<span className="separator">:</span>{secondPart}
-    </Box>
-  );
-}
